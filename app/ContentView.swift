f868b828ca46389aeaeba42896b63f368ad84482
ctih <onni09nevala@gmail.com>
2025-04-04 16:57:33 +0300

    Initial Commit

---
+++ app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by Onni Nevala on 4.4.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Onni Nevala on 4.4.2025.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
